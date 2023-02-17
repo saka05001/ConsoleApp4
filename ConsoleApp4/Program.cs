b2d4d6fc56f07fdb402bc93ee2d797b2867dace1
Pingvu <nhlpool27@gmail.com>
2023-02-17 14:57:29 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MetoderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv bredden");
+            int bredden = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv höjden");
+            int höjden = int.Parse(Console.ReadLine());
+
+
+            int area = Area(bredden, höjden);
+
+            Console.WriteLine("Arean är " + area);
+
+
+        }
+        static int Area(int bredden, int höjden)
+
+        {
+            return bredden * höjden;
+        }
+    }
+}
+
+
+
+            
+
+        
+  
+        
+
+
+
